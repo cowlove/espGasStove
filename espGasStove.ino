2fb1e7680b993237ae2656eb8d15f5288b4160b1
Jim Evans <jim@vheavy.com>
2022-11-20 13:57:44 -0800

    Use j.hz

--- espGasStove.ino
+++ espGasStove.ino
@@ -9,12 +9,12 @@ struct {
 } pins;
 
 TempSensor temp(pins.temp);
-PwmChannel pwm(pins.pwm, 50, 0, 2);
-Timer sec(2000), minute(60000), blink(100);
+PwmChannel pwm(pins.pwm, 50, 1, 2);
 
 CLI_VARIABLE_FLOAT(setTemp, 19.5);
 CLI_VARIABLE_FLOAT(hist, 0.15);
 CLI_VARIABLE_INT(useLED, 0);
+
 
 string testHook("hi");
 float lastTemp = 0.0;
@@ -27,27 +27,18 @@ void setup() {
 			pwm.setMs(atoi(m.str(1).c_str()));
 		return strfmt("%f", pwm.get()); 
 	});
-	j.cli.on("MINUTE", [](){ minute.alarmNow(); });
-	minute.alarmNow();	
 }
 
 void loop() {
 	j.run();
 
-	if (blink.tick()) { 
-		//OUT("%08d %6.3f SET: %6.3f, HIST: %6.3f LED: %d", 
-		//millis(), temp.readTemp(), (float)setTemp, (float)hist, (int)useLED);
-		if (useLED) 
-			digitalToggle(pins.led);
-		else
-			digitalWrite(pins.led, 0);
-	}
-
-	if (minute.tick()) {
+	if (j.secTick(60)) {
 		float t = temp.readTemp();
 		if (t > setTemp && (lastTemp == 0 || lastTemp <= setTemp)) { 
+			OUT("stove off");
 			pwm.setMs(900);
-		} else 	if (t < setTemp - hist && (lastTemp == 0 || lastTemp >= setTemp - hist)) { 
+		} else 	if (t < setTemp - hist && (lastTemp == 0 || lastTemp >= setTemp - hist)) {
+			OUT("stove on"); 
 			pwm.setMs(1700);
 			delay(3000);
 			pwm.setMs(1600);
